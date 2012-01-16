700ff9c3b8ef2a119fd9569c85641ed8b787e89d
Yu-Jie Lin <livibetter@gmail.com>
2012-01-16 23:54:13 +0800

    Import version 0.1
    Source from http://www.geekymonkey.com/programming/jquery/TextFill/jQuery-TextFill-0.1.zip

---
+++ jquery-textfill-0.1.js
@@ -0,0 +1,36 @@
+﻿; (function($) {
+	/**
+	* Resizes an inner element's font so that the inner element completely fills the outer element.
+	* @author Russ Painter WebDesign@GeekyMonkey.com
+	* @version 0.1
+	* @param {Object} Options which are maxFontPixels (default=40), innerTag (default='span')
+	* @return All outer elements processed
+	* @example <div class='mybigdiv filltext'><span>My Text To Resize</span></div>
+	*/
+	$.fn.textfill = function(options) {
+		var defaults = {
+			maxFontPixels: 40,
+			innerTag: 'span'
+		};
+		var Opts = jQuery.extend(defaults, options);
+		return this.each(function() {
+			var fontSize = Opts.maxFontPixels;
+			var ourText = $(Opts.innerTag + ':visible:first', this);
+			var maxHeight = $(this).height();
+			var maxWidth = $(this).width();
+			var textHeight;
+			var textWidth;
+			do {
+				ourText.css('font-size', fontSize);
+				textHeight = ourText.height();
+				textWidth = ourText.width();
+				fontSize = fontSize - 1;
+			} while ((textHeight > maxHeight || textWidth > maxWidth) && fontSize > 3);
+		});
+	};
+})(jQuery);
+
+$(document).ready(function() {
+	$('.jtextfill').textfill({ maxFontPixels: 36, innerTag: 'h1' });
+});
+
