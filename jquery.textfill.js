373664dbcfa7ec6b8c95962f476b130d68748b8e
Yu-Jie Lin <livibetter@gmail.com>
2012-01-17 00:16:35 +0800

    Fix images paths, rename plugin filename

--- jquery.textfill.js
+++ jquery.textfill.js
@@ -1,11 +1,19 @@
-﻿; (function($) {
+﻿/**
+ * @preserve  textfill
+ * @name      jquery.textfill.js
+ * @author    Russ Painter
+ * @version   0.1
+ * @date      05-27-2009
+ * @copyright (c) 2009 Russ Painter
+ * @license   MIT License
+ * @homepage  TBD
+ * @example   TBD
+*/
+; (function($) {
 	/**
 	* Resizes an inner element's font so that the inner element completely fills the outer element.
-	* @author Russ Painter WebDesign@GeekyMonkey.com
-	* @version 0.1
 	* @param {Object} Options which are maxFontPixels (default=40), innerTag (default='span')
 	* @return All outer elements processed
-	* @example <div class='mybigdiv filltext'><span>My Text To Resize</span></div>
 	*/
 	$.fn.textfill = function(options) {
 		var defaults = {
