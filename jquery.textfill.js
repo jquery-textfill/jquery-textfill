8e159573055836204b794591dc564a2d6497b8f0
Yu-Jie Lin <livibetter@gmail.com>
2012-01-17 00:34:11 +0800

    Cosmetic change, tab to 2 spaces

--- jquery.textfill.js
+++ jquery.textfill.js
@@ -10,35 +10,34 @@
  * @example   TBD
 */
 ; (function($) {
-	/**
-	* Resizes an inner element's font so that the inner element completely fills the outer element.
-	* @param {Object} Options which are maxFontPixels (default=40), innerTag (default='span')
-	* @return All outer elements processed
-	*/
-	$.fn.textfill = function(options) {
-		var defaults = {
-			maxFontPixels: 40,
-			innerTag: 'span'
-		};
-		var Opts = jQuery.extend(defaults, options);
-		return this.each(function() {
-			var fontSize = Opts.maxFontPixels;
-			var ourText = $(Opts.innerTag + ':visible:first', this);
-			var maxHeight = $(this).height();
-			var maxWidth = $(this).width();
-			var textHeight;
-			var textWidth;
-			do {
-				ourText.css('font-size', fontSize);
-				textHeight = ourText.height();
-				textWidth = ourText.width();
-				fontSize = fontSize - 1;
-			} while ((textHeight > maxHeight || textWidth > maxWidth) && fontSize > 3);
-		});
-	};
+  /**
+  * Resizes an inner element's font so that the inner element completely fills the outer element.
+  * @param {Object} Options which are maxFontPixels (default=40), innerTag (default='span')
+  * @return All outer elements processed
+  */
+  $.fn.textfill = function(options) {
+    var defaults = {
+      maxFontPixels: 40,
+      innerTag: 'span'
+    };
+    var Opts = jQuery.extend(defaults, options);
+    return this.each(function() {
+      var fontSize = Opts.maxFontPixels;
+      var ourText = $(Opts.innerTag + ':visible:first', this);
+      var maxHeight = $(this).height();
+      var maxWidth = $(this).width();
+      var textHeight;
+      var textWidth;
+      do {
+        ourText.css('font-size', fontSize);
+        textHeight = ourText.height();
+        textWidth = ourText.width();
+        fontSize = fontSize - 1;
+      } while ((textHeight > maxHeight || textWidth > maxWidth) && fontSize > 3);
+    });
+  };
 })(jQuery);
 
 $(document).ready(function() {
-	$('.jtextfill').textfill({ maxFontPixels: 36, innerTag: 'h1' });
+  $('.jtextfill').textfill({ maxFontPixels: 36, innerTag: 'h1' });
 });
-
