af46beaaee8c33d4a73f1a8fac05ad73fff28e13
Yu-Jie Lin <livibetter@gmail.com>
2012-05-25 11:31:43 +0800

    Find the max font size within width and height separately
    In previous algorithm, it finds the font size within width and height
    altogether, which may set minFontPixels incorrectly.
    
    This new algorithm may fix Issue 3.

--- jquery.textfill.js
+++ jquery.textfill.js
@@ -35,15 +35,30 @@
       
       var minFontPixels = Opts.minFontPixels;
       var maxFontPixels = Opts.maxFontPixels;
-      while (fontSize = Math.floor(minFontPixels + maxFontPixels) / 2,
-             minFontPixels <= maxFontPixels) {
+      while (minFontPixels < maxFontPixels - 1) {
+        fontSize = Math.floor((minFontPixels + maxFontPixels) / 2)
         ourText.css('font-size', fontSize);
-        if (ourText.height() < maxHeight && ourText.width() < maxWidth)
-          minFontPixels = fontSize + 1;
+        if (ourText.height() < maxHeight)
+          minFontPixels = fontSize;
         else
-          maxFontPixels = fontSize - 1;
+          maxFontPixels = fontSize;
       }
-      
+      var HfontSize = minFontPixels;
+
+      minFontPixels = Opts.minFontPixels;
+      maxFontPixels = Opts.maxFontPixels;
+      while (minFontPixels < maxFontPixels - 1) {
+        fontSize = Math.floor((minFontPixels + maxFontPixels) / 2)
+        ourText.css('font-size', fontSize);
+        if (ourText.width() < maxWidth)
+          minFontPixels = fontSize;
+        else
+          maxFontPixels = fontSize;
+      }
+      var WfontSize = minFontPixels
+
+      ourText.css('font-size', Math.min(HfontSize, WfontSize));
+
       // call callback on each result
       if (Opts.callback) Opts.callback(this);
     });
