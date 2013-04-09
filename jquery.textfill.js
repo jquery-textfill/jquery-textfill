5b1f0b7be8ca5557df73322651341204b8189061
Russ Painter <russp@geekymonkey.com>
2013-04-09 18:11:50 +0200

    Fix syntax error
    Related to console.debug not available checks.

--- jquery.textfill.js
+++ jquery.textfill.js
@@ -32,7 +32,7 @@
     var Opts = jQuery.extend(defaults, options);
 
     function _debug_sizing(prefix, ourText, maxHeight, maxWidth, minFontPixels, maxFontPixels) {
-      if (!Opts.debug || ) {
+      if (!Opts.debug) {
         return;
       }
       if (typeof console == 'undefined') {
